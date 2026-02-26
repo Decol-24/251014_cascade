7ccd7c6c3e3bbe02310d54abc9826418ad4c4665
Decol-24 <lww1969sg@gmail.com>
2026-02-26 11:12:09 +0900

    0226

--- CasStereoNet/test_time.py
+++ CasStereoNet/test_time.py
@@ -4,19 +4,22 @@ import argparse
 import argparse
 import torch
 import torch.backends.cudnn as cudnn
-import time
 from models import __models__, __loss__
 from utils import *
 
 @torch.no_grad()
-def evaluate_time(Net, imgL, imgR, device, warmup=30, times=50):
+def evaluate_time(Net, imgL, imgR, device, warmup=30, times=50, amp=False):
     Net = Net.to(device).eval()
     imgL = imgL.to(device)
     imgR = imgR.to(device)
 
     # warmup
-    for _ in range(warmup):
-        with torch.amp.autocast('cuda', enabled=True):
+    if amp:
+        for _ in range(warmup):
+            with torch.amp.autocast('cuda', enabled=True):
+                _ = Net(imgL, imgR)
+    else:
+        for _ in range(warmup):
             _ = Net(imgL, imgR)
     torch.cuda.synchronize()
 
@@ -24,18 +27,26 @@ def evaluate_time(Net, imgL, imgR, device, warmup=30, times=50):
     ender   = torch.cuda.Event(enable_timing=True)
 
     total_ms = 0.0
-    for _ in range(times):
-        starter.record()
-        with torch.amp.autocast('cuda', enabled=True):
+    if amp:
+        for _ in range(times):
+            starter.record()
+            with torch.amp.autocast('cuda', enabled=True):
+                _ = Net(imgL, imgR)
+            ender.record()
+            torch.cuda.synchronize()
+            total_ms += starter.elapsed_time(ender)
+    else:
+        for _ in range(times):
+            starter.record()
             _ = Net(imgL, imgR)
-        ender.record()
-        torch.cuda.synchronize()
-        total_ms += starter.elapsed_time(ender)
+            ender.record()
+            torch.cuda.synchronize()
+            total_ms += starter.elapsed_time(ender)
 
     avg_s = (total_ms / times) / 1000.0
     return avg_s
 
-@torch.no_grad()
+# @torch.no_grad()
 def evaluate_flops(Net,input,device,**kwargs):
     Net = Net.to(device)
     # input = input.to(device)
@@ -90,6 +101,7 @@ if __name__ == '__main__':
 
     # parse arguments
     args = parser.parse_args()
+    amp = False
 
     # model
     Net = __models__[args.model](
@@ -106,7 +118,7 @@ if __name__ == '__main__':
     imgL = torch.randn(1,3,544,960).to(args.device)
     imgR = torch.randn(1,3,544,960).to(args.device)
 
-    avg_run_time = evaluate_time(Net=Net,imgL=imgL,imgR=imgR,device=args.device)
+    avg_run_time = evaluate_time(Net=Net,imgL=imgL,imgR=imgR,device=args.device,amp=amp)
     total_flops,total_params = evaluate_flops(Net,input=(imgL,imgL),device=args.device)
 
     print(avg_run_time)
